6584727fa56471a5eb522e389ad37daa453c3a95
Espen Wiborg <espenhw@grumblesmurf.org>
2009-01-10 12:42:05 +0100

    Maven 3.0-alpha-1 is released; reinstate testMNG3838

--- src/test/java/org/grumblesmurf/jdemvn/MvnServerTest.java
+++ src/test/java/org/grumblesmurf/jdemvn/MvnServerTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.*;
 public class MvnServerTest
 {
     @Test
-    @Ignore("Waiting for resolution of MNG3838")
     public void testMNG3838() throws Exception {
         assertTrue(MvnServer.INSTANCE.run(System.getProperty("basedir") + "/src/test/resources/mng3838/pom.xml", "help:effective-pom"));
     }
