a7130d47c133821d00be9bf2e9e4fb7e11c9f5f2
Shane Xu <xusheng0711@gmail.com>
2013-07-02 18:41:30 +0800

    update prj.el template

--- prj.el
+++ prj.el
@@ -1,8 +1,14 @@
 (jde-project-file-version "1.0")
 (jde-set-variables
+ '(jde-build-function 'jde-mvn-build)
  '(jde-compile-option-command-line-args
    (quote ("-Xlint:all" "-Xlint:-serial"))))
 
 (require 'jde-mvn)
 (with-pom nil
   (jde-mvn-set-jde-variables :include-dependency-sources t))
+
+;; Map C-c C-v C-t to run current test
+(define-key jde-mode-map [remap jde-jdb-menu-debug-applet]
+  'jde-mvn-build-run-test)
+
